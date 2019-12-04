4b3239f87df34df44124daa128b479b357c4980f
Yernur <alimhanov.ernur@gmail.com>
2019-12-03 21:24:34 -0500

    Draft DataBaseManager and TableModel for Student

---
+++ src/main/java/DatabaseManager/DatabaseManager.java
@@ -0,0 +1,176 @@
+package DatabaseManager;
+
+import Entities.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class DatabaseManager {
+    EntityManager em;
+
+    public DatabaseManager(){
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "MyEclipseLinkExample" );
+        this.em = emfactory.createEntityManager();
+    }
+
+    public <E> void add(E entity){
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.flush();
+        em.getTransaction().commit();
+    }
+
+    public <E> void remove(E entity){
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.flush();
+        em.remove(entity);
+        em.flush();
+        em.getTransaction().commit();
+    }
+
+    public <E> void update(E entity){
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.flush();
+        em.getTransaction().commit();
+    }
+
+    public Assignment findAssignment(Integer id){
+        em.getTransaction().begin();
+        Assignment result = em.find(Assignment.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Category findCategory(Integer id){
+        em.getTransaction().begin();
+        Category result = em.find(Category.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public CategoryPercent findCategoryPercent(Integer id){
+        em.getTransaction().begin();
+        CategoryPercent result = em.find(CategoryPercent.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Course findCourse(Integer id){
+        em.getTransaction().begin();
+        Course result = em.find(Course.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Grades findGrades(Integer id){
+        em.getTransaction().begin();
+        Grades result = em.find(Grades.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Student findStudent(Integer id){
+        em.getTransaction().begin();
+        Student result = em.find(Student.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Professor findProfessor(Integer id){
+        em.getTransaction().begin();
+        Professor result = em.find(Professor.class,id);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public Semester findSemester(Integer id){
+        em.getTransaction().begin();
+        Semester result = em.find(Semester.class,id);
+        return result;
+    }
+
+    public Template findTemplate(Integer id){
+        em.getTransaction().begin();
+        Template result = em.find(Template.class,id);
+        return result;
+    }
+
+    public List<Assignment> getAllAssignments(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Assignment s");
+        List<Assignment> result = (List<Assignment>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Category> getAllCategories(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Category s");
+        List<Category> result = (List<Category>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<CategoryPercent> getAllCategoryPercents(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM CategoryPercent s");
+        List<CategoryPercent> result = (List<CategoryPercent>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Course> getAllCourses(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Course s");
+        List<Course> result = (List<Course>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Grades> getAllGrades(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Grades s");
+        List<Grades> result = (List<Grades>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Student> getAllStudents(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Student s");
+        List<Student> result = (List<Student>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Semester> getSemester(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Semester s");
+        List<Semester> result = (List<Semester>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Template> getTemplate(){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Template s");
+        List<Template> result = (List<Template>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public List<Course> getCoursesBySemester(Semester semester){
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT s FROM Course s WHERE s.semester = :sem");
+        q.setParameter("sem",semester);
+        List<Course> result = (List<Course>) q.getResultList();
+        em.getTransaction().commit();
+        return result;
+    }
+
+}
---
+++ src/main/java/TableModels/StudentTableModel.java
@@ -0,0 +1,41 @@
+package TableModels;
+
+import Entities.Student;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class StudentTableModel extends AbstractTableModel {
+    private String[] columnNames={"name","surname","buId","Email"};
+    private List<Student> students;
+
+    @Override
+    public int getRowCount() {
+        return this.students.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.columnNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Object result = null;
+        switch (columnIndex){
+            case 0:
+                result = this.students.get(rowIndex).getName();
+                break;
+            case 1:
+                result = this.students.get(rowIndex).getSurname();
+                break;
+            case 2:
+                result = this.students.get(rowIndex).getBuId();
+                break;
+            case 3:
+                result = this.students.get(rowIndex).getEmail();
+                break;
+        }
+        return result;
+    }
+}
