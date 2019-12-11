9f288cff196b4538707c3c71f2b93d66362ab4e3
Tian Gao <gaotian@bu.edu>
2019-12-10 23:18:35 -0500

    add student management function

--- src/main/java/GUI/StudentManagePage.java
+++ src/main/java/GUI/StudentManagePage.java
@@ -1,7 +1,14 @@
 package GUI;
 
+import Entities.*;
+import TableModels.StudentTableModel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StudentManagePage extends JFrame {
     private JLabel title;
@@ -13,6 +20,7 @@ public class StudentManagePage extends JFrame {
     private JButton delete;
     private JButton freeze;
     private JSeparator separator;
+    private List<Student> students;
 
     // Constructor
     public StudentManagePage() {
@@ -25,9 +33,6 @@ public class StudentManagePage extends JFrame {
         Font labelFont2 = new Font(Font.DIALOG, Font.BOLD, 20);
         className.setFont(labelFont2);
 
-        table = new JTable();
-        table.setAutoCreateRowSorter(true);
-        sp = new JScrollPane(table);
 
         back = new JButton("Back");
         add = new JButton("Add");
@@ -35,6 +40,95 @@ public class StudentManagePage extends JFrame {
         freeze = new JButton("Freeze");
 
         separator = new JSeparator();
+
+
+        // Test Data
+        Course cs591p1 = new Course("CS591 P1", null);
+
+        Student s1 = new GraduateStudent("Tian", "Gao", "U809", "gaotian@bu.edu");
+        Student s2 = new GraduateStudent("Xinyue", "Li", "U555", "xili33@bu.edu");
+        Student s3 = new GraduateStudent("Dou", "Bao", "U233", "doubao@bu.edu");
+
+        s1.setCourse(cs591p1);
+        s2.setCourse(cs591p1);
+        s3.setCourse(cs591p1);
+
+        java.util.List<Student> students = new LinkedList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+
+        Category hw = new Category("Homework", null);
+        Category project = new Category("Project", null);
+        Category exam = new Category("Exam", null);
+
+        CategoryPercent hwThis  = new CategoryPercent(0.5, hw, cs591p1);
+        CategoryPercent projThis  = new CategoryPercent(0.3, project, cs591p1);
+        CategoryPercent examThis  = new CategoryPercent(0.2, exam, cs591p1);
+
+        Assignment hw1 = new Assignment(0.5,  "Homework1", hwThis);
+        Assignment hw2 = new Assignment(0.5,  "Homework2", hwThis);
+        Assignment project1 = new Assignment(1,  "Project1", projThis);
+        Assignment midterm = new Assignment(0.6,  "Midterm", examThis);
+        Assignment finalExam = new Assignment(0.5,  "Final Exam", examThis);
+
+        java.util.List<Assignment> homeworks = new LinkedList<>();
+        java.util.List<Assignment> projects = new LinkedList<>();
+        java.util.List<Assignment> exams = new LinkedList<>();
+        homeworks.add(hw1);
+        homeworks.add(hw2);
+        projects.add(project1);
+        exams.add(midterm);
+        exams.add(finalExam);
+        hwThis.setAssignments(homeworks);
+        projThis.setAssignments(projects);
+        examThis.setAssignments(exams);
+
+        java.util.List<CategoryPercent> categoryPercents = new LinkedList<>();
+        categoryPercents.add(hwThis);
+        categoryPercents.add(projThis);
+        categoryPercents.add(examThis);
+
+        cs591p1.setCategoryPercents(categoryPercents);
+
+        Grades hw1Grade  = new Grades();
+        hw1Grade.setAssignment(hw1);
+        hw1Grade.setGrade(95);
+        hw1Grade.setGraded(true);
+        Grades hw2Grade  = new Grades();
+        hw2Grade.setAssignment(hw2);
+        hw2Grade.setGrade(60);
+        hw2Grade.setGraded(true);
+        Grades proj1Grade  = new Grades();
+        proj1Grade.setAssignment(project1);
+        proj1Grade.setGrade(88);
+        proj1Grade.setGraded(true);
+        Grades midtermGrade  = new Grades();
+        midtermGrade.setAssignment(midterm);
+        midtermGrade.setGrade(100);
+        midtermGrade.setGraded(true);
+        Grades finalGrade  = new Grades();
+        finalGrade.setAssignment(finalExam);
+        finalGrade.setGrade(0);
+        finalGrade.setGraded(false);
+
+        List<Grades> grades = new LinkedList<>();
+        grades.add(hw1Grade);
+        grades.add(hw2Grade);
+        grades.add(proj1Grade);
+        grades.add(midtermGrade);
+        grades.add(finalGrade);
+
+        s1.setGrades(grades);
+        s2.setGrades(grades);
+        s3.setGrades(grades);
+
+        this.students = students;
+
+        StudentTableModel studentTableModel = new StudentTableModel(students);
+        table = new JTable(studentTableModel);
+        table.setAutoCreateRowSorter(true);
+        sp = new JScrollPane(table);
 
 
         title.setBounds(50, 20, 800, 50);
@@ -79,6 +173,57 @@ public class StudentManagePage extends JFrame {
 
         add.addActionListener(e -> {
             AddStudentPage addStudentPage = new AddStudentPage();
+        });
+
+        delete.addActionListener(e -> {
+            ((StudentTableModel)table.getModel()).deleteRow(table.getSelectedRow());
+            JOptionPane.showMessageDialog(null, "Delete Successfully!");
+        });
+
+        freeze.addActionListener(e -> {
+            ((StudentTableModel)table.getModel()).changeFreeze(table.getSelectedRow());
+            if (((StudentTableModel)table.getModel()).getFreeze(table.getSelectedRow())) {
+                freeze.setText("Unfreeze");
+            } else {
+                freeze.setText("Freeze");
+            }
+            freeze.repaint();
+            JOptionPane.showMessageDialog(null, "Status Change Successfully!");
+        });
+
+        table.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (table.getSelectedRow() != -1) {
+                    if (((StudentTableModel)table.getModel()).getFreeze(table.getSelectedRow())) {
+                        freeze.setText("Unfreeze");
+                    } else {
+                        freeze.setText("Freeze");
+                    }
+                    freeze.repaint();
+                    System.out.println("!!");
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
         });
 
     }
--- src/main/java/TableModels/StudentTableModel.java
+++ src/main/java/TableModels/StudentTableModel.java
@@ -1,12 +1,14 @@
 package TableModels;
 
+import Entities.Grades;
 import Entities.Student;
 
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import java.util.List;
 
 public class StudentTableModel extends AbstractTableModel {
-    private String[] columnNames={"name","surname","buId","Email"};
+    private String[] columnNames={"First Name","Last Name","BU ID","Email", "Is Freeze"};
     private List<Student> students;
 
     public StudentTableModel(List<Student> students){
@@ -21,6 +23,10 @@ public class StudentTableModel extends AbstractTableModel {
     @Override
     public int getColumnCount() {
         return this.columnNames.length;
+    }
+
+    public String getColumnName(int col) {
+        return this.columnNames[col];
     }
 
     @Override
@@ -39,7 +45,60 @@ public class StudentTableModel extends AbstractTableModel {
             case 3:
                 result = this.students.get(rowIndex).getEmail();
                 break;
+            case 4:
+                if (this.students.get(rowIndex).isFreezed()) {
+                    result = "True";
+                } else {
+                    result = "False";
+                }
         }
         return result;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                students.get(rowIndex).setName((String) aValue);
+                break;
+            case 1:
+                students.get(rowIndex).setSurname((String) aValue);
+                break;
+            case 2:
+                students.get(rowIndex).setBuId((String) aValue);
+                break;
+            case 3:
+                students.get(rowIndex).setEmail((String) aValue);
+                break;
+        }
+        // TODO: 2019-12-10 add database function 
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return columnIndex != 4;
+    }
+
+    @Override
+    public void fireTableDataChanged() {
+        super.fireTableDataChanged();
+    }
+
+    public void deleteRow(int rowIndex) {
+        students.remove(rowIndex);
+        fireTableDataChanged();
+        // TODO: 2019-12-10  add database function
+    }
+
+    public void changeFreeze(int rowIndex) {
+        if (students.get(rowIndex).isFreezed()) {
+            students.get(rowIndex).setFreezed(false);
+        } else {
+            students.get(rowIndex).setFreezed(true);
+        }
+    }
+
+    public boolean getFreeze(int rowIndex) {
+        return students.get(rowIndex).isFreezed();
     }
 }
